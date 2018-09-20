034029cd0eb0f923c71a2d55546981307a34195a
Sparsh Karki <sparshkarki@gmail.com>
2018-09-20 13:05:15 +0545

    customised dashboard

---
+++ resources/views/admin_dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('master.master')
+
+@section('title')
+ SmartDoko | Home page | Online Shopping Nepal
+@endsection
+
+@section('container')
+
+
+<h1> This is admin Dashboard page</h1>
+
+
+
+@endsection
