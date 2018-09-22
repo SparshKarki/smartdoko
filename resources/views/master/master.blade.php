365a7cd42330ddf74158a4dbc8e94dc9d32e75e2
Sparsh Karki <sparshkarki@gmail.com>
2018-09-22 14:34:05 +0545

    top nav bar

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -55,9 +55,9 @@
 						<li class="active"><a href="#">Home</a></li>
 						<li><a href="#">Hot Deals</a></li>
 						<li><a href="#">Categories</a></li>
-						<li><a href="#">Laptops</a></li>
-						<li><a href="#">Smartphones</a></li>
-						<li><a href="#">Cameras</a></li>
+						<li><a href="#">Men</a></li>
+						<li><a href="#">Women</a></li>
+						<li><a href="#">Kids</a></li>
 						<li><a href="#">Accessories</a></li>
 					</ul>
 					<!-- /NAV -->
